6062de23e39ab2074216823a353dce0702368d1c
Gagan Chawla <thegaganchawla@gmail.com>
2024-04-19 01:15:29 +0530

    checkpoint 2

--- apps/word_count.py
+++ apps/word_count.py
@@ -12,57 +12,40 @@ spark = SparkSession.builder.appName(
 ).getOrCreate()
 sc = spark.sparkContext
 
-##
-# A - 2
-# B - 1
-# C - 5
-# D - 2
-# E - 2
+# A=2, B=1, C=5, D=2, E=2
+rdd = sc.textFile("./data/names.txt", 6)
+df = rdd.map(lambda name: (name[0], name)).toDF()
 
-
-#3
-# E, C
-
-#4
-# A
-# D
-# B, C
-
-#5
-# D
-# E, C
-
-# result
-# A
-# D
-# B, C, E
-# Null
-# Null
-
-df = sc.textFile("./data/names.txt", 6)
 df.collect()
-# check workers for shuffle files - should be 0
-
 df.foreachPartition(lambda rows: print_partition(rows))
 
-df = df.map(lambda name: (name[0], name))
-# same here. Pipelined
+rdd = df.rdd.groupByKey(numPartitions=6)
+rdd.foreachPartition(lambda rows: print_partition_2(rows))
 
-df = df.groupByKey(numPartitions=6)
-df.collect()
-df.foreachPartition(lambda rows: (print(r) for r in rows))
-
-df = df.mapValues(lambda names: len(set(names)))
-df.collect()
+rdd = rdd.mapValues(lambda names: len(set(names)))
+rdd.collect()
 
 time.sleep(60)
 
+# from operator import add
+# rdd = sc.textFile("./data/names.txt", 6)
+# df = rdd.map(lambda name: (name[0], 1)).toDF()
+# df.rdd.reduceByKey(add).collect()
+
 
 def print_partition(rows):
-    sys.stdout.write("\nPrinting Partition\n")
+    sys.stdout.write("\nPrinting Partition v1\n")
     # print("Printing Partition")
     for row in rows:
         # print(row)
         sys.stdout.write(f"{row}\n")
     sys.stdout.flush()
 
+
+def print_partition_2(rows):
+    sys.stdout.write("\nPrinting Partition v2\n")
+    # print("Printing Partition")
+    for row in rows:
+        # print(row)
+        sys.stdout.write(f"{row[0]} --> {row[1].data}\n")
+    sys.stdout.flush()
