af68ad961ef6a0d925f1a631953e82831bcf81ba
Gagan Chawla <thegaganchawla@gmail.com>
2024-05-28 13:00:27 +0530

    moved print function to the top

--- apps/word_count.py
+++ apps/word_count.py
@@ -6,6 +6,24 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import StructType, StringType, StructField
 
 
+def print_partition(rows):
+    sys.stdout.write("\nPrinting Partition v1\n")
+    # print("Printing Partition")
+    for row in rows:
+        # print(row)
+        sys.stdout.write(f"{row}\n")
+    sys.stdout.flush()
+
+
+def print_partition_2(rows):
+    sys.stdout.write("\nPrinting Partition v2\n")
+    # print("Printing Partition")
+    for row in rows:
+        # print(row)
+        sys.stdout.write(f"{row[0]} --> {row[1].data}\n")
+    sys.stdout.flush()
+
+
 spark = SparkSession.builder.appName(
     "Word Count test"
 ).config(
@@ -42,21 +60,3 @@ time.sleep(60)
 # rdd = sc.textFile("./data/names.txt", 6)
 # df = rdd.map(lambda name: (name[0], 1)).toDF()
 # df.rdd.reduceByKey(add).collect()
-
-
-def print_partition(rows):
-    sys.stdout.write("\nPrinting Partition v1\n")
-    # print("Printing Partition")
-    for row in rows:
-        # print(row)
-        sys.stdout.write(f"{row}\n")
-    sys.stdout.flush()
-
-
-def print_partition_2(rows):
-    sys.stdout.write("\nPrinting Partition v2\n")
-    # print("Printing Partition")
-    for row in rows:
-        # print(row)
-        sys.stdout.write(f"{row[0]} --> {row[1].data}\n")
-    sys.stdout.flush()
